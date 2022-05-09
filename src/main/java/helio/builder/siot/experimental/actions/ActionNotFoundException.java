8830fc4ba533c009d3bdc1f47555c530cc112e79
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-09 21:32:38 +0200

    Created new custom error for actions

---
+++ src/main/java/helio/builder/siot/experimental/actions/ActionNotFoundException.java
@@ -0,0 +1,11 @@
+package helio.builder.siot.experimental.actions;
+
+public class ActionNotFoundException extends Exception{
+
+	private static final long serialVersionUID = 12238394L;
+
+	public ActionNotFoundException(String actionType) {
+		super("Action class " + actionType + " not found.");
+	}
+	
+}
