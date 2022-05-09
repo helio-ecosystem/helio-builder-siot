2b7b6be7c3fb58c7c220ffb7a5a973907b31aa68
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-09 21:32:54 +0200

    Created HttpRequestAction

---
+++ src/main/java/helio/builder/siot/experimental/actions/HttpRequestAction.java
@@ -0,0 +1,20 @@
+package helio.builder.siot.experimental.actions;
+
+import com.google.gson.JsonObject;
+
+public class HttpRequestAction implements Action {
+
+	public static final String NAME = "Http";
+	
+	@Override
+	public void configure(JsonObject configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String run(String values) {
+		return null;
+	}
+
+}
