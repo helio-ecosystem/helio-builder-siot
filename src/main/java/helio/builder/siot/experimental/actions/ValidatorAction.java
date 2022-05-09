aac07401f098b2f918ae183a96734d191e8b991e
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-09 21:33:04 +0200

    Created ValidatorAction

---
+++ src/main/java/helio/builder/siot/experimental/actions/ValidatorAction.java
@@ -0,0 +1,20 @@
+package helio.builder.siot.experimental.actions;
+
+import com.google.gson.JsonObject;
+
+public class ValidatorAction implements Action {
+
+	public static final String NAME = "Validator";
+	
+	@Override
+	public void configure(JsonObject configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String run(String values) {
+		return null;
+	}
+
+}
