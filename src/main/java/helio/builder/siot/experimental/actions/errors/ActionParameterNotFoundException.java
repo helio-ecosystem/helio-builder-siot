ce4260982070c59939b905ddf6bd39dc465a29c2
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-10 21:23:46 +0200

    Create ActionParameterNotFoundException.java

---
+++ src/main/java/helio/builder/siot/experimental/actions/errors/ActionParameterNotFoundException.java
@@ -0,0 +1,11 @@
+package helio.builder.siot.experimental.actions.errors;
+
+public class ActionParameterNotFoundException extends Exception{
+
+	private static final long serialVersionUID = 32499341424L;
+
+	public ActionParameterNotFoundException(String actionParam) {
+		super("Parameter '" + actionParam + "' not found in action directive.");
+	}
+	
+}
