423c58cce6bec1d1f5b74b6f768092863fc1ea6a
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-13 21:30:51 +0200

    [Test] Filled ValidatorActionTests test02

--- src/test/java/tests/actions/ValidatorActionTests.java
+++ src/test/java/tests/actions/ValidatorActionTests.java
@@ -2,7 +2,18 @@ package tests.actions;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.Test;
+
+import helio.blueprints.TranslationUnit;
+import helio.blueprints.exceptions.ExtensionNotFoundException;
+import helio.blueprints.exceptions.IncompatibleMappingException;
+import helio.blueprints.exceptions.IncorrectMappingException;
+import helio.blueprints.exceptions.TranslationUnitExecutionException;
+import tests.TestUtils;
 
 /**
  * Set of test which validates the validator action module.
@@ -23,8 +34,17 @@ public class ValidatorActionTests {
 	 * The JSON source is correct and the validator verifies it.
 	 */
 	@Test
-	public void test02_ValidateCorrectJSONDataWithJSONSChema() {
-		assertTrue(false);
+	public void test02_ValidateCorrectJSONDataWithJSONSChema() throws Exception {
+		try {
+			ExecutorService service = Executors.newFixedThreadPool(4);
+			TranslationUnit unit = TestUtils.build("./src/test/resources/action-samples/validator/json-valid.txt");
+			String result = TestUtils.runUnit(unit, service);
+			service.shutdownNow();
+			System.out.println(result);
+			assertTrue(true);
+		} catch (Exception e) {
+			assertTrue(e.getMessage(), false);
+		}
 	}
 	
 	/**
