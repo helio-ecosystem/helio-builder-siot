b2a6e5072db75ba1beffae19c851202130f60b31
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-13 21:24:55 +0200

    Create XmlValidatorAction.java
    To be implemented

---
+++ src/main/java/helio/builder/siot/experimental/actions/module/validator/XmlValidatorAction.java
@@ -0,0 +1,22 @@
+package helio.builder.siot.experimental.actions.module.validator;
+
+import com.google.gson.JsonObject;
+
+import helio.blueprints.Action;
+
+public class XmlValidatorAction implements Action {
+
+	public static final String TAG = "XmlValidator";
+	
+	@Override
+	public void configure(JsonObject configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String run(String values) {
+		return null;
+	}
+
+}
