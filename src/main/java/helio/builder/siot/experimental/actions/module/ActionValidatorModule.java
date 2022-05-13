7555730843241e7e613136ee5e58611688542c9c
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-13 21:23:22 +0200

    Filled build method for action validator module

--- src/main/java/helio/builder/siot/experimental/actions/module/ActionValidatorModule.java
+++ src/main/java/helio/builder/siot/experimental/actions/module/ActionValidatorModule.java
@@ -2,6 +2,8 @@ package helio.builder.siot.experimental.actions.module;
 
 import helio.blueprints.Action;
 import helio.builder.siot.experimental.actions.errors.ActionNotFoundException;
+import helio.builder.siot.experimental.actions.module.validator.JsonValidatorAction;
+import helio.builder.siot.experimental.actions.module.validator.XmlValidatorAction;
 
 public class ActionValidatorModule implements ActionModule {
 
@@ -14,7 +16,19 @@ public class ActionValidatorModule implements ActionModule {
 	
 	@Override
 	public Action build(String type) throws ActionNotFoundException {
-		throw new ActionNotFoundException(type);
+		Action action;
+
+		if (type.equalsIgnoreCase(JsonValidatorAction.TAG)) {
+			action = new JsonValidatorAction();
+		}
+		else if (type.equalsIgnoreCase(XmlValidatorAction.TAG)) {
+			action = new XmlValidatorAction();
+		}
+		else {
+			throw new ActionNotFoundException(type);
+		}
+		
+		return action;
 	}
 
 }
