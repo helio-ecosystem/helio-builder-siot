73887b0d8f3f4b0c5a9d3262156f343de0ced2d1
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-14 16:27:58 +0200

    Update ActionDirective.java

--- src/main/java/helio/builder/siot/experimental/directives/ActionDirective.java
+++ src/main/java/helio/builder/siot/experimental/directives/ActionDirective.java
@@ -26,20 +26,20 @@ public class ActionDirective implements TemplateDirectiveModel {
 	private static final String PARAM_NAME_TYPE = "type";
 	private static final String PARAM_NAME_CONF = "conf";
 	private static final String PARAM_NAME_DATA = "data";
-	
+
 	@Override
 	public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
 			throws TemplateException, IOException {
 		// Retrieve data from directive
 		ParamsHolder paramsHolder = getParameters(params);
-        
+
 		// Validate directive parameters
-        try {
-        	validateParameters(paramsHolder);
-        } catch (ActionParameterNotFoundException e) {
-        	throw new TemplateModelException(e.getMessage());
-        }
-        
+		try {
+			validateParameters(paramsHolder);
+		} catch (ActionParameterNotFoundException e) {
+			throw new TemplateModelException(e.getMessage());
+		}
+
 		// Execute action
 		try {
 			Action action = ActionBuilder.instance().build(paramsHolder.type);
@@ -54,46 +54,45 @@ public class ActionDirective implements TemplateDirectiveModel {
 			throw new TemplateModelException(e.getMessage());
 		}
 	}
-	
+
 	private ParamsHolder getParameters(Map params) {
 		ParamsHolder paramsHolder = new ParamsHolder();
 		Iterator paramIter = params.entrySet().iterator();
-		
-        while (paramIter.hasNext()) {
-        	Map.Entry ent = (Map.Entry) paramIter.next();
 
-            String paramName = (String) ent.getKey();
-            String paramValue = (String) ent.getValue();
+		while (paramIter.hasNext()) {
+			Map.Entry ent = (Map.Entry) paramIter.next();
 
-            if (paramName.equals(PARAM_NAME_TYPE)) {
+			String paramName = (String) ent.getKey();
+			String paramValue = (String) ent.getValue();
+
+			if (paramName.equals(PARAM_NAME_TYPE)) {
 				paramsHolder.type = paramValue;
-			}
-			else if (paramName.equals(PARAM_NAME_CONF)) {
+			} else if (paramName.equals(PARAM_NAME_CONF)) {
 				paramsHolder.configuration = JsonParser.parseString(paramValue).getAsJsonObject();
-			}
-			else if (paramName.equals(PARAM_NAME_DATA)) {
+			} else if (paramName.equals(PARAM_NAME_DATA)) {
 				paramsHolder.data = paramValue;
 			}
 		}
-        
-        return paramsHolder;
+
+		return paramsHolder;
 	}
-	
+
 	private boolean validateParameters(ParamsHolder params) throws ActionParameterNotFoundException {
 		if (Strings.isNullOrEmpty(params.type)) {
-        	throw new ActionParameterNotFoundException("type");
-        }
-        /*
-		else if (params.configuration.isJsonNull() || params.configuration.entrySet().isEmpty()) {
-        	throw new ActionParameterNotFoundException("configuration");
-        }
-		*/
-        else if (Strings.isNullOrEmpty(params.data)) {
-        	throw new ActionParameterNotFoundException("data");
-    	}
+			throw new ActionParameterNotFoundException("type");
+		}
+		/*
+		 * else if (params.configuration.isJsonNull() ||
+		 * params.configuration.entrySet().isEmpty()) {
+		 * throw new ActionParameterNotFoundException("configuration");
+		 * }
+		 */
+		else if (Strings.isNullOrEmpty(params.data)) {
+			throw new ActionParameterNotFoundException("data");
+		}
 		return true;
 	}
-	
+
 	private class ParamsHolder {
 		String type;
 		JsonObject configuration;
