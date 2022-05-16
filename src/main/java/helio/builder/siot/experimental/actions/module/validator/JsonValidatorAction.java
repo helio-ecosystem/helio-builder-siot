7f2d8da074b42bc949ac2373ac8bfd49c5701dd8
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-16 21:13:59 +0200

    Update JsonValidatorAction.java
    Throws JsonSyntaxException if json is invalid

--- src/main/java/helio/builder/siot/experimental/actions/module/validator/JsonValidatorAction.java
+++ src/main/java/helio/builder/siot/experimental/actions/module/validator/JsonValidatorAction.java
@@ -1,32 +1,28 @@
 package helio.builder.siot.experimental.actions.module.validator;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
 import helio.blueprints.Action;
 
+/**
+ * Action class for Validator Module encharge of validating JSON schemas.
+ * 
+ * @author Emilio
+ * 
+ */
 public class JsonValidatorAction implements Action {
 
 	public static final String TAG = "JsonValidator";
 
 	@Override
 	public void configure(JsonObject configuration) {
-		
+
 	}
 
 	@Override
 	public String run(String values) {
-		String validatedData = null;
-
-		try {
-			JsonObject json = JsonParser.parseString(values).getAsJsonObject();
-			validatedData = json.getAsString();
-		} catch (JsonParseException e) {
-			e.printStackTrace();
-		}
-
-		return validatedData;
+		return JsonParser.parseString(values).getAsJsonObject().toString();
 	}
 
 }
