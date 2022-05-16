344bbc8248a5b6d52cf15bc7749fde1c34d3c326
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-16 21:18:13 +0200

    Created tests for action directive

---
+++ src/test/java/tests/actions/ActionDirectiveTests.java
@@ -0,0 +1,104 @@
+package tests.actions;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import helio.builder.siot.experimental.actions.ActionBuilder;
+import helio.builder.siot.experimental.actions.errors.ActionParameterNotFoundException;
+
+/**
+ * Set of test which validates the action directive.
+ * 
+ * @author Emilio
+ *
+ */
+public class ActionDirectiveTests {
+
+	private final String DIR_RESOURCES = "./src/test/resources/action-samples/directive/";
+
+	@BeforeClass
+	public static void addMockupModule() {
+		ActionBuilder.instance().addActionModule(ActionDirectiveTestUtils.getMockupModule());
+	}
+
+	@AfterClass
+	public static void removeMockupModule() {
+		ActionBuilder.instance().removeModule(ActionDirectiveTestUtils.getMockupModule());
+	}
+
+	/**
+	 * Validates that 'type' parameter in action directive is mandatory.
+	 */
+	@Test
+	public void test01_ActionDirectiveWithoutType() {
+		try {
+			ActionDirectiveTestUtils.executeTestWithTemplate(DIR_RESOURCES + "01_action-no-type.txt");
+			assertTrue("Test is expected to fail", false);
+		} catch (Exception e) {
+			String exceptionMessage = e.getMessage().split(":")[2];
+			String exceptionExpected = ActionParameterNotFoundException.class.getCanonicalName();
+			if (ActionDirectiveTestUtils.isExceptionMessageExpected(exceptionMessage, exceptionExpected)) {
+				assertTrue(true);
+			} else {
+				assertTrue(e.getMessage(), false);
+			}
+		}
+	}
+
+	/**
+	 * Validates that 'conf' parameter in action directive is optional.
+	 */
+	@Test
+	public void test02_ActionDirectiveWithoutConfiguration() {
+		try {
+			ActionDirectiveTestUtils.executeTestWithTemplate(DIR_RESOURCES + "02_action-no-conf.txt");
+			assertTrue(true);
+		} catch (Exception e) {
+			assertTrue( false);
+		}
+	}
+
+	/**
+	 * Validates that 'data' parameter in action directive is mandatory.
+	 */
+	@Test
+	public void test03_ActionDirectiveWithoutData() {
+		try {
+			ActionDirectiveTestUtils.executeTestWithTemplate(DIR_RESOURCES + "03_action-no-data.txt");
+			assertTrue("Test is expected to fail", false);
+		} catch (Exception e) {
+			String exceptionMessage = e.getMessage().split(":")[2];
+			String exceptionExpected = ActionParameterNotFoundException.class.getCanonicalName();
+
+			if (ActionDirectiveTestUtils.isExceptionMessageExpected(exceptionMessage, exceptionExpected)) {
+				assertTrue(true);
+			} else {
+				assertTrue(e.getMessage(), false);
+			}
+		}
+	}
+
+	/**
+	 * Validates that 'type' parameter in action directive exists in modules.
+	 */
+	@Test
+	public void test04_ActionDirectiveWithTypeWhichNotExists() {
+		try {
+			ActionDirectiveTestUtils.executeTestWithTemplate(DIR_RESOURCES + "04_action-type-not-exists.txt");
+			assertTrue("Test is expected to fail", false);
+		} catch (Exception e) {
+			String exceptionMessage = e.getMessage().split(":")[2];
+			String exceptionExpected = ActionParameterNotFoundException.class.getCanonicalName();
+
+			if (ActionDirectiveTestUtils.isExceptionMessageExpected(exceptionMessage, exceptionExpected)) {
+				assertTrue(true);
+			} else {
+				assertTrue(e.getMessage(), false);
+			}
+		}
+	}
+
+}
