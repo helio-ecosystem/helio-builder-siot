06c0afd36317aa9296cabbe21d9931ecb5224fce
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-17 20:59:28 +0200

    Updated configure & run methods from Http Request Action

--- src/main/java/helio/builder/siot/experimental/actions/module/request/HttpRequestAction.java
+++ src/main/java/helio/builder/siot/experimental/actions/module/request/HttpRequestAction.java
@@ -6,17 +6,44 @@ import helio.blueprints.Action;
 
 public class HttpRequestAction implements Action {
 
-	public static final String NAME = "Http";
-	
+	public static final String TAG = "HttpRequest";
+
+	private String method;
+	private String url;
+
 	@Override
 	public void configure(JsonObject configuration) {
-		// TODO Auto-generated method stub
-		
+		if (configuration == null) {
+			return;
+		}
+		if (configuration.has("url")) {
+			this.url = configuration.get("url").getAsString().strip();
+		}
+		if (configuration.has("method")) {
+			this.method = configuration.get("method").getAsString().strip();
+		}
 	}
 
 	@Override
 	public String run(String values) {
-		return null;
+		String result = null;
+
+		if (this.url != null && this.method != null) {
+			if (this.method.equalsIgnoreCase("get")) {
+				result = HttpRequestBuilder.instance().get(this.url);
+			}
+			else if (this.method.equalsIgnoreCase("post")) {
+				result = HttpRequestBuilder.instance().post(this.url, values);
+			}
+			else if (this.method.equalsIgnoreCase("put")) {
+				result = HttpRequestBuilder.instance().put(this.url, values);
+			}
+			else if (this.method.equalsIgnoreCase("delete")) {
+				result = HttpRequestBuilder.instance().get(this.url);
+			}
+		}
+
+		return result;
 	}
 
 }
