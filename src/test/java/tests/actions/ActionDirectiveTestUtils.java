97df28e203f9c5fb019ac1318787b0d4be6fee4a
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-24 21:12:03 +0200

    [Test] Added dynamic template for test

--- src/test/java/tests/actions/ActionDirectiveTestUtils.java
+++ src/test/java/tests/actions/ActionDirectiveTestUtils.java
@@ -1,5 +1,6 @@
 package tests.actions;
 
+import java.io.PrintWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,10 +20,11 @@ import tests.TestUtils;
  */
 public class ActionDirectiveTestUtils {
 
-	public static final String DIR_DIRECTIVE_RESOURCES = "./src/test/resources/action-samples/directive/";
-	public static final String DIR_VALIDATOR_RESOURCES = "./src/test/resources/action-samples/validator/";
-	public static final String DIR_REQUEST_RESOURCES = "./src/test/resources/action-samples/request/";
-	public static final String DIR_SCENARIOS_RESOURCES = "./src/test/resources/action-samples/scenarios/";
+	public static final String DIR_RESOURCES = "./src/test/resources/action-samples/";
+	public static final String DIR_DIRECTIVE_RESOURCES = DIR_RESOURCES + "directive/";
+	public static final String DIR_VALIDATOR_RESOURCES = DIR_RESOURCES + "validator/";
+	public static final String DIR_REQUEST_RESOURCES = DIR_RESOURCES + "request/";
+	public static final String DIR_SCENARIOS_RESOURCES = DIR_RESOURCES + "scenarios/";
 
 	public static MockupActionTestModule getMockupModule() {
 		return new MockupActionTestModule();
@@ -34,6 +36,16 @@ public class ActionDirectiveTestUtils {
 		String result = TestUtils.runUnit(unit, service);
 		service.shutdownNow();
 		return result;
+	}
+
+	public static String executeTestWithStringTemplate(String content) throws Exception {
+		// Creates a file with content
+		PrintWriter writer = new PrintWriter(DIR_RESOURCES + "template-test.txt");
+		writer.println(content);
+		writer.close();
+
+		// Executes test
+		return executeTestWithTemplate(DIR_RESOURCES + "template-test.txt");
 	}
 
 	public static boolean isExceptionMessageExpected(String exceptionMessage, String exceptionExpected) {
