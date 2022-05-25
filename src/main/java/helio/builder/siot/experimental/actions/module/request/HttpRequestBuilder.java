13db244ccf2e10590c6b26b861e9cd62f69d7b54
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-25 21:27:19 +0200

    Added timeout for request & controlled exceptions for error codes

--- src/main/java/helio/builder/siot/experimental/actions/module/request/HttpRequestBuilder.java
+++ src/main/java/helio/builder/siot/experimental/actions/module/request/HttpRequestBuilder.java
@@ -1,23 +1,24 @@
 package helio.builder.siot.experimental.actions.module.request;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
-
-import javax.management.RuntimeErrorException;
 
 public class HttpRequestBuilder {
     
     private static HttpRequestBuilder singleton = null;
     private HttpClient client;
+    private Duration defaultTimeout;
 
     private HttpRequestBuilder() {
         this.client = HttpClient.newHttpClient();
+        this.defaultTimeout = Duration.of(30, ChronoUnit.SECONDS);
     }
 
     public static HttpRequestBuilder instance() {
@@ -27,24 +28,32 @@ public class HttpRequestBuilder {
         return singleton;
     }
 
+    public Duration getDefaultTimeout() {
+        return defaultTimeout;
+    }
+
+    public void setDefaultTimeout(Duration defaultTimeout) {
+        this.defaultTimeout = defaultTimeout;
+    }
+
     public String get(String url, Map<String, String> headers) {
         HttpRequest.Builder requestBuilder = createRequestBuilder(url, headers);
-        return executeRequest(requestBuilder.GET().build());
+        return executeRequest(requestBuilder.GET().timeout(defaultTimeout).build());
     }
 
     public String post(String url, String data, Map<String, String> headers) {
         HttpRequest.Builder requestBuilder = createRequestBuilder(url, headers);
-        return executeRequest(requestBuilder.POST(BodyPublishers.ofString(data)).build());
+        return executeRequest(requestBuilder.POST(BodyPublishers.ofString(data)).timeout(defaultTimeout).build());
     }
 
     public String put(String url, String data, Map<String, String> headers) {
         HttpRequest.Builder requestBuilder = createRequestBuilder(url, headers);
-        return executeRequest(requestBuilder.PUT(BodyPublishers.ofString(data)).build());
+        return executeRequest(requestBuilder.PUT(BodyPublishers.ofString(data)).timeout(defaultTimeout).build());
     }
 
     public String delete(String url, Map<String, String> headers) {
         HttpRequest.Builder requestBuilder = createRequestBuilder(url, headers);
-        return executeRequest(requestBuilder.DELETE().build());
+        return executeRequest(requestBuilder.DELETE().timeout(defaultTimeout).build());
     }
 
     private HttpRequest.Builder createRequestBuilder(String url, Map<String, String> headers) {
@@ -62,9 +71,18 @@ public class HttpRequestBuilder {
 
         try {
         	HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        	if (response != null && response.statusCode() >= 200 && response.statusCode() < 300) {
-        		result = response.body();
-        	}
+        	if (response != null) {
+                if (response.statusCode() >= 200 && response.statusCode() < 300) {
+                    result = response.body();
+                }
+                // Custom error codes
+                else  if (response.statusCode() >= 400 && response.statusCode() < 500) {
+                    throw new Exception(String.valueOf(response.statusCode()));
+                }
+                else  if (response.statusCode() >= 500 && response.statusCode() < 600) {
+                    throw new Exception(String.valueOf(response.statusCode()));
+                }
+            }
 		}
         catch (Exception e) {
         	throw new RuntimeException("HttpRequestBuilderException: " + e.getMessage());
