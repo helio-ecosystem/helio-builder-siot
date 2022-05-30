0cbf4f640c9ba3640af3fdfd607e19750f6bdece
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-30 21:32:14 +0200

    [Test | Validator] Implemented XML tests

--- src/test/java/tests/actions/ValidatorActionTests.java
+++ src/test/java/tests/actions/ValidatorActionTests.java
@@ -1,10 +1,18 @@
 package tests.actions;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 
+import helio.blueprints.exceptions.ActionException;
+import helio.builder.siot.experimental.actions.errors.JsonValidatorException;
+import helio.builder.siot.experimental.actions.errors.XmlValidatorException;
+import org.apache.jena.base.Sys;
 import org.junit.Test;
+import tests.TestUtils;
 
 /**
  * Set of test which validates the validator action module.
@@ -18,18 +26,15 @@ public class ValidatorActionTests {
 	 * The JSON source is incorrect and the validator throws a JsonSyntaxException.
 	 */
 	@Test
-	public void test01_ValidateInvalidJSONDataWithJSONSChema() {
+	public void test01_ValidateInvalidJsonDataWithJsonSchema() {
 		try {
 			ActionDirectiveTestUtils.executeTestWithTemplate(
-				ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "01_json-template.txt");
+					ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "01_json-template.txt");
 			assertTrue(false);
 		} catch (Exception e) {
-			String exceptionExpected = JsonSyntaxException.class.getCanonicalName();
-			if (ActionDirectiveTestUtils.isExceptionMessageExpected(e.getMessage(), exceptionExpected)) {
-				assertTrue(true);
-			} else {
-				assertTrue(e.getMessage(), false);
-			}
+			String expected = JsonValidatorException.class.getCanonicalName().strip();
+			String obtained = e.getMessage().split(":")[2].strip();
+			assertEquals(expected, obtained);
 		}
 	}
 
@@ -37,11 +42,12 @@ public class ValidatorActionTests {
 	 * The JSON source is correct and the validator verifies it.
 	 */
 	@Test
-	public void test02_ValidateCorrectJSONDataWithJSONSChema() {
+	public void test02_ValidateCorrectJsonDataWithJsonSchema() {
 		try {
-			ActionDirectiveTestUtils.executeTestWithTemplate(
-				ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "02_json-template.txt");
-			assertTrue(true);
+			String expected = TestUtils.readFile(ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "json-valid.json");
+			String obtained = ActionDirectiveTestUtils.executeTestWithTemplate(
+					ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "02_json-template.txt");
+			assertEquals(JsonParser.parseString(expected), JsonParser.parseString(obtained));
 		} catch (Exception e) {
 			assertTrue(e.getMessage(), false);
 		}
@@ -51,32 +57,67 @@ public class ValidatorActionTests {
 	 * The JSON source is correct but the validator expected a XML data.
 	 */
 	@Test
-	public void test03_ValidateJSONDataWithXMLChema() {
-		assertTrue(false);
+	public void test03_ValidateJsonDataWithXmlSchema() {
+		try {
+			String obtained = ActionDirectiveTestUtils.executeTestWithTemplate(
+					ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "03_json-template.txt");
+
+			assertTrue(ActionDirectiveTestUtils.TAG_TEST_FAIL, false);
+		} catch (Exception e) {
+			String expected = XmlValidatorException.class.getCanonicalName().strip();
+			String obtained = e.getMessage().split(":")[2].strip();
+			assertEquals(expected, obtained);
+		}
 	}
 
 	/**
 	 * The XML source is incorrect and the validator throws an error.
 	 */
 	@Test
-	public void test04_ValidateInvalidXMLDataWithXMLSChema() {
-		assertTrue(false);
+	public void test04_ValidateInvalidXmlDataWithXmlSchema() {
+		try {
+			String obtained = ActionDirectiveTestUtils.executeTestWithTemplate(
+					ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "04_xml-template.txt");
+
+			assertTrue(ActionDirectiveTestUtils.TAG_TEST_FAIL, false);
+		} catch (Exception e) {
+			String expected = XmlValidatorException.class.getCanonicalName().strip();
+			String obtained = e.getMessage().split(":")[2].strip();
+			assertEquals(expected, obtained);
+		}
 	}
 
 	/**
 	 * The XML source is correct and the validator verifies it.
 	 */
 	@Test
-	public void test05_ValidateCorrectXMLDataWithXMLSChema() {
-		assertTrue(false);
+	public void test05_ValidateCorrectXmlDataWithXmlSchema() {
+		try {
+			String expected = TestUtils.readFile(ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "xml-valid.xml");
+			String obtained = ActionDirectiveTestUtils.executeTestWithTemplate(
+					ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "05_xml-template.txt");
+
+			assertEquals(expected.strip(), obtained.strip());
+		} catch (Exception e) {
+			assertTrue(e.getMessage(), false);
+		}
 	}
 
 	/**
 	 * The XML source is correct but the validator expected a JSON data.
 	 */
 	@Test
-	public void test06_ValidateXMLDataWithJSONSChema() {
-		assertTrue(false);
+	public void test06_ValidateXmlDataWithJsonSchema() {
+		try {
+			String obtained = ActionDirectiveTestUtils.executeTestWithTemplate(
+					ActionDirectiveTestUtils.DIR_VALIDATOR_RESOURCES + "06_xml-template.txt");
+
+			assertTrue(ActionDirectiveTestUtils.TAG_TEST_FAIL, false);
+		} catch (Exception e) {
+			String expected = JsonValidatorException.class.getCanonicalName().strip();
+			String obtained = e.getMessage().split(":")[2].strip();
+			assertEquals(expected, obtained);
+		}
 	}
 
 }
