978f632c4d2100cc69cbad94d1b4d6af71d7e1f1
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-30 21:34:08 +0200

    Update JsonValidatorAction.java

--- src/main/java/helio/builder/siot/experimental/actions/module/validator/JsonValidatorAction.java
+++ src/main/java/helio/builder/siot/experimental/actions/module/validator/JsonValidatorAction.java
@@ -4,6 +4,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import helio.blueprints.Action;
+import helio.blueprints.exceptions.ActionException;
+import helio.builder.siot.experimental.actions.errors.JsonValidatorException;
 
 /**
  * Action class for Validator Module encharge of validating JSON schemas.
@@ -21,8 +23,15 @@ public class JsonValidatorAction implements Action {
 	}
 
 	@Override
-	public String run(String values) {
-		return JsonParser.parseString(values).getAsJsonObject().toString();
+	public String run(String values) throws ActionException {
+		String result = "";
+		try {
+			result = JsonParser.parseString(values).getAsJsonObject().toString();
+		}
+		catch (Exception e) {
+			throw new JsonValidatorException("Invalid JSON format: " + e.getMessage());
+		}
+		return result;
 	}
 
 }
