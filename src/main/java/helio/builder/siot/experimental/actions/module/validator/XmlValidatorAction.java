3d889fba91c4f8229f3264558888caa0305f1b11
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-05-30 21:34:42 +0200

    Implemented action for xml validator

--- src/main/java/helio/builder/siot/experimental/actions/module/validator/XmlValidatorAction.java
+++ src/main/java/helio/builder/siot/experimental/actions/module/validator/XmlValidatorAction.java
@@ -3,6 +3,13 @@ package helio.builder.siot.experimental.actions.module.validator;
 import com.google.gson.JsonObject;
 
 import helio.blueprints.Action;
+import helio.blueprints.exceptions.ActionException;
+import helio.builder.siot.experimental.actions.errors.XmlValidatorException;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
 
 public class XmlValidatorAction implements Action {
 
@@ -10,13 +17,22 @@ public class XmlValidatorAction implements Action {
 	
 	@Override
 	public void configure(JsonObject configuration) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public String run(String values) {
-		return null;
+	public String run(String values) throws ActionException {
+		try {
+			DocumentBuilderFactory fctr = DocumentBuilderFactory.newInstance();
+			DocumentBuilder bldr = fctr.newDocumentBuilder();
+			InputSource insrc = new InputSource(new StringReader(values));
+			bldr.parse(insrc);
+		}
+		catch (Exception e) {
+			throw new XmlValidatorException("Invalid XML format: " + e.getMessage());
+		}
+
+		return values;
 	}
 
 }
